18ded5f292b0285d134389b793e43e76a892d7e8
Milos Mancic <miolos@gmail.com>
2022-06-29 21:07:02 +0200

    primer line, pie, bar chart

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// LINE CHART 
 const lineChart = document.getElementById('lineChart');
 
 const labels = [
@@ -36,6 +37,55 @@ new Chart(lineChart, {
                 label: 'Novi pregledi',
                 data: [1199 , 1314 , 1147 , 1403 , 1217 , 1015 , 1138 , 1272 , 957 , 862 , 716 , 712 , 610 , 585 , 847 , 744 , 756 , 781],
                 borderColor: 'rgba(235, 134, 52, 1)',
+            }
+        ]
+    }
+});
+
+// PIE CHART 
+const pieChart = document.getElementById('pieChart');
+
+new Chart(pieChart, {
+    type: 'pie',
+    data: {
+        labels: ['Srbija','BiH', 'Hrvatska', 'S. Makedonija', 'CG', 'Ostali'],
+        datasets: [
+            {
+                label: 'Drzave',
+                data: [49.6, 20.3, 9.5, 3.6, 2.6, 14.4],
+                backgroundColor: [
+                    'rgba(52, 235, 143, 1)',
+                    'rgba(235, 201, 52, 1)',
+                    'rgba(52, 153, 235, 1)',
+                    'rgba(235, 52, 119, 1)',
+                    'rgba(186, 52, 235, 1)',
+                    'rgba(52, 153, 170, 1)'
+                ]
+            }
+        ]
+    }
+});
+
+// BAR CHART 
+const barChart = document.getElementById('barChart');
+
+new Chart(barChart, {
+    type: 'bar',
+    data: {
+        labels: ['13 - 17', '18- 24', '25-34', '35-44', '45-54', '55-64', '65+'],
+        datasets: [
+            {
+                label: 'Publika po godinama',
+                data: [1.7, 43.3, 34.4, 13.6, 5.6, 1.2, 0.3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(155, 159, 64, 1)'
+                ]
             }
         ]
     }
